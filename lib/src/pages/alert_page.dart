4094a492bbab6d804fc99883f006a03d83f45c3c
Luis Felipe <rl.luisfe@gmail.com>
2020-04-26 18:09:52 -0400

    alert component

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -7,6 +7,17 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Alert Page'),
       ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            _showAlert(context);
+          },
+          child: Text('Show alert'),
+          shape: StadiumBorder(),
+          color: Colors.blue,
+          textColor: Colors.white,
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.pop(context);
@@ -14,5 +25,36 @@ class AlertPage extends StatelessWidget {
         child: Icon(Icons.arrow_back),
       ),
     );
+  }
+
+  void _showAlert(BuildContext context) {
+    showDialog(
+        context: context,
+        barrierDismissible: true,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('Title'),
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0)),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Text('This is the content of the alert dialog...'),
+                SizedBox(
+                  height: 50.0,
+                ),
+                FlutterLogo(size: 60.0)
+              ],
+            ),
+            actions: <Widget>[
+              FlatButton(
+                  onPressed: () => Navigator.of(context).pop(),
+                  child: Text('Cancel')),
+              FlatButton(
+                  onPressed: () => Navigator.of(context).pop(),
+                  child: Text('Ok'))
+            ],
+          );
+        });
   }
 }
--- lib/src/pages/card_page.dart
+++ lib/src/pages/card_page.dart
@@ -12,14 +12,6 @@ class CardPage extends StatelessWidget {
             SizedBox(height: 30.0),
             _imageCard(),
             SizedBox(height: 30.0),
-            _simpleCard(),
-            SizedBox(height: 30.0),
-            _imageCard(),
-            SizedBox(height: 30.0),
-            _simpleCard(),
-            SizedBox(height: 30.0),
-            _imageCard(),
-            SizedBox(height: 30.0),
           ]),
     );
   }
