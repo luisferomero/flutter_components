61698d3bc65976e8a26ce407cc841245357edf60
Luis Felipe <rl.luisfe@gmail.com>
2020-04-26 16:11:23 -0400

    cards components

---
+++ lib/src/pages/card_page.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+class CardPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Cards')),
+      body: ListView(
+          padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 15.0),
+          children: <Widget>[
+            _simpleCard(),
+            SizedBox(height: 30.0),
+            _imageCard(),
+            SizedBox(height: 30.0),
+            _simpleCard(),
+            SizedBox(height: 30.0),
+            _imageCard(),
+            SizedBox(height: 30.0),
+            _simpleCard(),
+            SizedBox(height: 30.0),
+            _imageCard(),
+            SizedBox(height: 30.0),
+          ]),
+    );
+  }
+
+  Widget _simpleCard() {
+    return Card(
+      elevation: 8.0,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+      child: Column(
+        children: <Widget>[
+          ListTile(
+            leading: Icon(
+              Icons.image,
+              size: 30.0,
+            ),
+            title: Text('Card Title'),
+            subtitle: Text(
+                'lorem ipsun lorem ipsun lorem ipsun lorem ipsun lorem ipsun lorem ipsun lorem ipsun lorem ipsun'),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              FlatButton(
+                onPressed: () {},
+                child: Text('Cancel'),
+              ),
+              FlatButton(
+                onPressed: () {},
+                child: Text('Ok'),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _imageCard() {
+    return Card(
+      clipBehavior: Clip.antiAlias,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+      elevation: 5.0,
+      child: Column(
+        children: <Widget>[
+          FadeInImage(
+            image: NetworkImage(
+                'https://upload.wikimedia.org/wikipedia/commons/3/35/Neckertal_20150527-6384.jpg'),
+            placeholder: AssetImage('assets/loader.gif'),
+            fadeInDuration: Duration(milliseconds: 200),
+            height: 300.0,
+            fit: BoxFit.cover,
+          ),
+          Container(
+            padding: EdgeInsets.all(10),
+            child: Text('Card Title'),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter_components/src/pages/alert_page.dart';
 import 'package:flutter_components/src/pages/avatar_page.dart';
+import 'package:flutter_components/src/pages/card_page.dart';
 import 'package:flutter_components/src/pages/home_page.dart';
 
 Map<String, WidgetBuilder> getRoutes() {
@@ -9,5 +10,6 @@ Map<String, WidgetBuilder> getRoutes() {
     '/'     : (BuildContext context) => HomePage(),
     'alert' : (BuildContext context) => AlertPage(),
     'avatar': (BuildContext context) => AvatarPage(),
+    'card'  : (BuildContext context) => CardPage(),
   };
 }
